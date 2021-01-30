cc2a7c54c7d3ceb39eacda245acd377846b4014c
Zack Jameson <zacharymjameson@gmail.com>
2021-01-30 11:01:05 -0500

    base functionality for artist description wikiAPI request and fixed other functions to handle queries with & in them

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const lastFMkey = "ff2a30e86dd95313c30416251bfab2f2";
 const lastFMurl = "http://ws.audioscrobbler.com/2.0/";
+const wikiURL= 'https://en.wikipedia.org/w/api.php?';
 
 //Formatting Query Parameters
 function formatQueryParams(params) {
   const queryItems = Object.keys(params).map((key) => `${key}=${params[key]}`);
   return queryItems.join("&");
+}
+
+//Formatting Query Params handling & searches
+function encodeQueryParams(params) {
+  const encodedItems = Object.keys(params).map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
+  return encodedItems.join('&');
 }
 
 //Display Top Albums
@@ -14,11 +21,11 @@ function displayTopAlbums(responseJson){
   let topAlbums = ``;
 
   for(let i = 0; i < responseJson.topalbums.album.length; i++){
-    topAlbums += `<div class="item">
+    topAlbums += `<li class="item">
                   <h5>${responseJson.topalbums.album[i].name}</h5>
                   <p><a href="${responseJson.topalbums.album[i].url}">Listen to the Album</a></p>
                   <p>Total Plays: ${responseJson.topalbums.album[i].playcount}</p>
-                  </div>`
+                  </li>`
   }
   $('#top-albums').append(topAlbums);
 }
@@ -43,7 +50,8 @@ function displayEvents(responseJson) {
 
 //SeatGeek Upcoming Events Request
 function getUpcomingEvents(artistInput) {
-  artistInput = artistInput.replace(/\s/g, "-");
+  artistInput = artistInput.replace(/\s+/g, "-");
+  artistInput = artistInput.replace('&', "and");
 
   const seatGeekURL = `https://api.seatgeek.com/2/events?client_id=MjE1MTkyODd8MTYxMTc5OTA3Ny40NDYwODkz&performers.slug=${artistInput}`;
   console.log(seatGeekURL);
@@ -61,6 +69,25 @@ function getUpcomingEvents(artistInput) {
     });
 }
 
+//Wikipedia Artist Description Request
+function getArtistDescription(artistInput){
+  artistInput = artistInput.replace(/\s/g, "");
+
+  const artistParams = {
+    action: 'query',
+    format: 'json',
+    prop: 'extracts',
+    titles: artistInput,
+    exintro: 1
+  };
+
+    descriptionQuery = encodeQueryParams(artistParams);
+    const descriptionURL = wikiURL + '?' + descriptionQuery;
+
+    console.log(descriptionURL);
+
+}
+
 //LastFM Request for Top Albums Limiting to 5
 function getTopAlbums(artistInput) {
   artistInput = artistInput.replace(/\s/g, "");
@@ -73,7 +100,7 @@ function getTopAlbums(artistInput) {
     limit: 5,
   };
 
-  const albumQuery = formatQueryParams(albumParams);
+  const albumQuery = encodeQueryParams(albumParams);
   const albumURL = lastFMurl + "?" + albumQuery;
 
   console.log(albumURL);
@@ -104,6 +131,7 @@ function watchButton() {
     getUpcomingEvents(musicianSearch);
     displayArtistName(musicianSearch);
     getTopAlbums(musicianSearch);
+    getArtistDescription(musicianSearch);
   });
 }
 
